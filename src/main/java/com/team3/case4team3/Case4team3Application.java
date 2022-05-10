01edceb2552f822582bbfca30424084d198294a4
ThaiDuong <duongmoe174@gmail.com>
2022-05-10 16:20:33 +0700

    first git

---
+++ src/main/java/com/team3/case4team3/Case4team3Application.java
@@ -0,0 +1,13 @@
+package com.team3.case4team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case4team3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Case4team3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/team3/case4team3/Case4team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.case4team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Case4team3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
